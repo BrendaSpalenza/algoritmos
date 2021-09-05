f4236b59128cb96127bde27c0139b56914411644
BrendaSpalenza <brendaspalenza07@gmail.com>
2021-09-05 14:32:59 -0300

    Contador de média de notas utilizando if e else, e na aula teórica foi explicado and, or e not, mas utilizei o and.

---
+++ contadorDeNotas.py
@@ -0,0 +1,7 @@
+m1 = float(input('Digite a nota da 1º matéria'))
+m2 = float(input('Digite a nota da 2º matéria'))
+m3 = float(input('Digite a nota da 3º matéria'))
+if m1 < 7 and m2 < 7 and m3 < 7:
+    print('O aluno não foi aprovado...')
+else:
+    print('O aluno está aprovado!')
