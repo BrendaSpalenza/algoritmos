a9c364fbce4819b3b0e03a0258b00343af032ba4
BrendaSpalenza <brendaspalenza07@gmail.com>
2021-09-05 19:11:43 -0300

    Função que calcula o fatorial passando um número.

---
+++ fatoração.py
@@ -0,0 +1,24 @@
+def valida_int(pergunta, min, max):
+    x = int(input(pergunta))
+    while ((x < min) or (x > max)):
+        x = int(input(pergunta))
+    return x
+
+def fatorial(num):
+    """
+    Calcula a fatorial
+    :param num: Número a ser fatorado
+    :return: Número fatorado
+    """
+
+    fat = 1
+    if num == 0:
+        return fat
+    for i in range(1, num+1, 1):
+        fat *= i # fat = fat*i
+    return fat
+
+
+x = valida_int('Digite um valor para calcular a fatorial: ', 0, 99999)
+print('{}! = {}' .format(x, fatorial(x)))
+help(fatorial)
