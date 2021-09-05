f9bca37d85c1eb1b78efd638fba62bfbeae66062
Leonardo de Souza de Oliveira <leonardo.souza.oliveira@ibm.com>
2021-09-05 14:18:48 -0300

    funcao que coloca uma borda a uma mensagem

---
+++ funcaoBorda.py
@@ -0,0 +1,15 @@
+def borda(s1):
+    tam = len(s1)
+    print('+', '=' * tam, '+')
+    print('!', s1, '!')
+    print('+', '=' * tam, '+')
+
+
+nome = (input('Coloque uma frase: '))
+borda(nome)
+
+
+#borda('  olá')
+#borda('Oii, meu nome é Brenda')
+#borda('Meu nome é Brenda, eu tenho 20 anos, moro em Colatina - ES, CEP 29706-070')
+
