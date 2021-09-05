f9a2bb13ae6a38ce25601e1df4bc6eabf9164412
BrendaSpalenza <brendaspalenza07@gmail.com>
2021-09-05 20:18:45 -0300

    Cria arquivos de listagem de jogos.

---
+++ criadorDeArquivos.py
@@ -0,0 +1,77 @@
+def valida_int(pergunta, min, max):
+    x = int(input(pergunta))
+    while ((x < min) or (x > max)):
+        x = int(input(pergunta))
+    return x
+
+def criarArquivo(nomeArquivo):
+    try:
+        a = open(nomeArquivo, 'wt+')
+        a.close()
+    except:
+        print('Erro na criação do arquivo.')
+    else:
+        print('Arquivo {} foi criado com sucesso!\n' .format(nomeArquivo))
+
+def existearquivo(nomeArquivo):
+    try:
+        a = open(nomeArquivo, 'rt')
+        a.close()
+    except FileNotFoundError:
+        return False
+    else:
+        return True
+
+def cadastrarJogo(nomeArquivo, nomeJogo, nomeVideogame):
+    try:
+        a = open(nomeArquivo, 'at')
+    except:
+        print('Erro ao abrir o arquivo..')  #Caso de erro
+    else:
+        a.write('{};{}\n' .format(nomeJogo, nomeVideogame))  #Caso não de erro
+    finally:
+        a.close() #Tanto faz, executará de qualquer maneira
+
+def listarArquivo(nomeArquivo):
+    try:
+        a = open(nomeArquivo, 'rt')
+    except:
+        print('Erro ao ler o arquivo.')
+    else:
+        print(a.read())
+    finally:
+        a.close()
+
+
+
+#Programa principal
+arquivo = 'games.txt'
+
+
+if existearquivo(arquivo):
+    print('Arquivo localizado no computador')
+else:
+    print('Arquivo inexistente!')
+    criarArquivo(arquivo)
+
+while True:
+    print('MENU')
+    print('1 -  Cadastrar um novo item')
+    print('2 - Listar os cadastros')
+    print('3 - Sair')
+
+
+    op = valida_int('Escolha a opção desejada: ', 1, 3)
+    if op == 1:
+        print('Opção de cadastrar novo item selecionado....\n')
+        nomeJogo = input('Nome do jogo: ')
+        nomeVideogame = input('Nome do videogame: ')
+        cadastrarJogo(arquivo, nomeJogo, nomeVideogame)
+
+    elif op == 2:
+        print('Opção de listar selecionada....\n')
+        listarArquivo(arquivo)
+
+    elif op == 3:
+        print('Encerrando o programa ....')
+        break
